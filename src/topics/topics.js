e4ca445e4b6aadcc7342821caa562aa43e232b72
AndrasSzemes <andras@szemes.eu>
2021-06-01 15:42:44 +0200

    note: aws, cloud computing

--- src/topics/topics.js
+++ src/topics/topics.js
@@ -2155,7 +2155,7 @@ Examples of daily life:
       Hybrid deployment: Mix of previous two. One option is to have on-premise apps, and have a backup at the cloud.
 
   -----
-  **Design principles***
+  **Design principles*** CROPS
       Aims: secure, durable, high-performing
 
     Aim: **cost optimization***
@@ -2165,17 +2165,18 @@ Examples of daily life:
     Aim: **reliability*** (recover from service discruptions, often byself)
       Test disaster recovery
       Have duplicate copies of resources
+      
+    Aim: **operational excellence*** (ability to run and monitor systems while constantly improving processes and procedures)
+      document everything
+      operational processes constantly refine
 
-    Aim: **efficient*** (ability to use computing resources to adjust to system requirements)
+    Aim: **performance efficient*** (ability to use computing resources to adjust to system requirements)
     
     Aim: **security***
       best practices should be automated
       data should always be protected
       enable traceability
 
-    Aim: **operational excellence*** (ability to run and monitor systems while constantly improving processes and procedures)
-      document everything
-      operational processes constantly refine
 
 
 
@@ -2213,6 +2214,301 @@ Diving into AWS
 
     `,
   },
+
+  empty: {
+    title: "AWS Certified Cloud Practitioner Exam",
+    related: [],
+    text: `
+Domain of Exam
+  Domain 1: Cloud Concepts 26%
+  Domain 2: Security and Compliance 25%
+  Domain 3: Technology 33%
+  Domain 4: Billing and Pricing 16%
+TOTAL 100%
+
+**Domain 1: Cloud Concepts***
+
+1.1 Define the AWS Cloud and its value proposition
+  Amazon Web Services (AWS) is the world’s most comprehensive and broadly adopted cloud platform, offering over 200 fully featured services from data centers globally. 
+
+  Define the benefits of the AWS cloud including:
+    <> Security
+      - AWS is architected to be the most flexible and secure cloud computing environment available today.
+      - core infrastructure is built to satisfy the security requirements for the military, global banks, and other high-sensitivity organizations
+      - backed by a deep set of cloud security tools, with 230 security, compliance, and governance services and features. AWS supports 90 security standards and compliance certifications, and all 117 AWS services that store customer data offer the ability to encrypt that data.
+    <> Reliability
+    <> High Availability
+    <> Elasticity
+      don’t have to over-provision resources up front, instead provision the amount of resources that you actually need can scale these resources up or down to instantly grow and shrink capacity as your business needs change
+    <> Agility
+      - access to a broad range of technologies
+      - quickly spin up resources as you need them
+      - deploy technology services in a matter of minutes (<=> data centers, it may take weeks to obtain new resources)
+    <> Pay-as-you go pricing or Scalability
+        For each resource, you have to pay only for the usage. For example, if a Lambda Function is used for 3 hours a day, you only pay for these hours and not the whole day.
+        You don't have to analyse the amount and specifications of resources beforehand of an application deployment, because on the way when there is a larger usage, resources can be updated as needed, even scaled back when no longer needed.
+
+    <> Global Reach
+      The global footprint of the AWS Cloud enables you to deploy applications to customers around the world quickly, while providing them with low latency. This means that even if you are located in a different part of the world than your customers, customers are able to access your applications with minimal delays. 
+    <> Economy of scale
+      By using cloud computing, you can achieve a lower variable cost than you can get on your own.
+      Because usage from hundreds of thousands of customers can aggregate in the cloud, providers, such as AWS, can achieve higher economies of scale. The economy of scale translates into lower pay-as-you-go prices.
+
+      Explain how the AWS cloud allows users to focus on business value
+    <> Shifting technical resources to revenue-generating activities as opposed to managing
+    infrastructure
+
+    AWS services: compute, storage, network security, blockchain, machine learning, AI, robot development, video production
+    On-demand: have the neccessary resources available when you need it
+
+    Why are there so many services? - Undifferentiated heavy lifting of IT
+    Cost saving
+      The cloud allows you to trade capital expenses (such as data centers and physical servers) for variable expenses, and only pay for IT as you consume it. Plus, the variable expenses are much lower than what you would pay to do it yourself because of the economies of scale.
+    No other cloud provider offers as many Regions with multiple Availability Zones connected by low latency, high throughput, and highly redundant networking. AWS has 80 Availability Zones within 25 geographic regions around the world, and has announced plans for 18 more Availability Zones and 6 more AWS Regions in Australia, India, Indonesia, Spain, Switzerland, and United Arab Emirates (UAE).
+
+1.2 Identify aspects of AWS Cloud economics
+  Define items that would be part of a Total Cost of Ownership proposal
+o Understand the role of operational expenses (OpEx)
+o Understand the role of capital expenses (CapEx)
+o Understand labor costs associated with on-premises operations
+o Understand the impact of software licensing costs when moving to the cloud
+                Identify
+o Right-sized infrastructure
+which operations will reduce costs by moving to the cloud
+o Benefits of automation
+o Reduce compliance scope (for example, reporting)
+o Managed services (for example, RDS, ECS, EKS, DynamoDB)
+
+1.3 Explain the different cloud architecture design principles   Explain the design principles
+  <> Design for failure
+  <> Decouple components versus monolithic architecture o Implement elasticity in the cloud versus on-premises o Think parallel
+
+**Domain 2: Security and Compliance***
+
+2.1 Define the AWS shared responsibility model
+  Recognize the elements of the Shared Responsibility Model
+  Describe the customer’s responsibly on AWS
+o Describe how the customer’s responsibilities may shift depending on the service used (for example with RDS, Lambda, or EC2)
+  Describe AWS responsibilities
+
+2.2 Define AWS Cloud security and compliance concepts
+  Identify where to find AWS compliance information
+o Locations of lists of recognized available compliance controls (for example, HIPPA, SOCs)
+o Recognize that compliance requirements vary among AWS services   At a high level, describe how customers achieve compliance on AWS
+o Identify different encryption options on AWS (for example, In transit, At rest)
+  Describe who enables encryption on AWS for a given service
+  Recognize there are services that will aid in auditing and reporting
+o Recognize that logs exist for auditing and monitoring (do not have to understand the logs)
+o Define Amazon CloudWatch, AWS Config, and AWS CloudTrail   Explain the concept of least privileged access
+
+2.3 Identify AWS access management capabilities
+  Understand the purpose of User and Identity Management
+o Access keys and password policies (rotation, complexity) o Multi-Factor Authentication (MFA)
+o AWS Identity and Access Management (IAM)
+• Groups/users
+• Roles
+• Policies, managed policies compared to custom policies
+o Tasks that require use of root accounts Protection of root accounts
+
+2.4 Identify resources for security support
+  Recognize there are different network security capabilities
+o Native AWS services (for example, security groups, Network ACLs, AWS WAF)
+o 3rd party security products from the AWS Marketplace
+  Recognize there is documentation and where to find it (for example, best practices,
+whitepapers, official documents)
+
+o AWS Knowledge Center, Security Center, security forum, and security blogs o Partner Systems Integrators
+  Know that security checks are a component of AWS Trusted Advisor
+
+
+  Domain 3: Technology
+3.1 Define methods of deploying and operating in the AWS Cloud
+  Identify at a high level different ways of provisioning and operating in the AWS cloud
+o Programmatic access, APIs, SDKs, AWS Management Console, CLI, Infrastructure as Code
+  Identify different types of cloud deployment models o All in with cloud/cloud native
+o Hybrid
+o On-premises
+  Identify connectivity options o VPN
+o AWS Direct Connect o Public internet
+3.2 Define the AWS global infrastructure
+  Describe the relationships among Regions, Availability Zones, and Edge Locations
+  Describe how to achieve high availability through the use of multiple Availability Zones
+o Recall that high availability is achieved by using multiple Availability Zones
+o Recognize that Availability Zones do not share single points of failure   Describe when to consider the use of multiple AWS Regions
+o Disaster recovery/business continuity o Low latency for end-users
+o Data sovereignty
+  Describe at a high level the benefits of Edge Locations o Amazon CloudFront
+o AWS Global Accelerator 3.3 Identify the core AWS services
+
+  
+
+o
+•
+Describe the categories of services on AWS (compute, storage, network, database) Identify AWS compute services
+o Recognize there are different compute families
+o Recognize the different services that provide compute (for example, AWS Lambda
+compared to Amazon Elastic Container Service (Amazon ECS), or Amazon EC2, etc.) o Recognize that elasticity is achieved through Auto Scaling
+o Identify the purpose of load balancers
+Identify different AWS storage services
+o Describe Amazon S3
+o Describe Amazon Elastic Block Store (Amazon EBS) o Describe Amazon S3 Glacier
+o Describe AWS Snowball
+o Describe Amazon Elastic File System (Amazon EFS) o Describe AWS Storage Gateway
+Identify AWS networking services o Identify VPC
+o Identify security groups
+o Identify the purpose of Amazon Route 53 o Identify VPN, AWS Direct Connect
+Identify different AWS database services
+o Install databases on Amazon EC2 compared to AWS managed databases
+
+  o Identify Amazon RDS
+o Identify Amazon DynamoDB o Identify Amazon Redshift
+3.4 Identify resources for technology support
+  Recognize there is documentation (best practices, whitepapers, AWS Knowledge Center,
+forums, blogs)
+  Identify
+o AWS Abuse
+the various levels and scope of AWS support
+o AWS support cases
+o Premium support
+o Technical Account Managers
+  Recognize there is a partner network (marketplace, third-party) including Independent Software Vendors and System Integrators
+  Identify sources of AWS technical assistance and knowledge including professional services, solution architects, training and certification, and the Amazon Partner Network
+  Identify the benefits of using AWS Trusted Advisor Domain 4: Billing and Pricing
+4.1 Compare and contrast the various pricing models for AWS (for example, On-Demand Instances, Reserved Instances, and Spot Instance pricing)
+  Identify scenarios/best fit for On-Demand Instance pricing
+  Identify scenarios/best fit for Reserved-Instance pricing
+o Describe Reserved-Instances flexibility
+o Describe Reserved-Instances behavior in AWS Organizations
+  Identify scenarios/best fit for Spot Instance pricing
+4.2 Recognize the various account structures in relation to AWS billing and pricing
+  Recognize that consolidated billing is a feature of AWS Organizations
+  Identify how multiple accounts aid in allocating costs across departments
+4.3 Identify resources available for billing support
+  Identify ways to get billing support and information
+o Cost Explorer, AWS Cost and Usage Report, Amazon QuickSight, third-party partners, and AWS Marketplace tools
+o Open a billing support case
+o The role of the Concierge for AWS Enterprise Support Plan customers
+  Identify where to find pricing information on AWS services
+o AWS Simple Monthly Calculator o AWS Services product pages
+o AWS Pricing API
+  Recognize that alarms/alerts exist
+  Identify how tags are used in cost allocation
+
+
+  Appendix
+Which key tools, technologies, and concepts might be covered on the exam?
+The following is a non-exhaustive list of the tools and technologies that could appear on the exam. This list is subject to change and is provided to help you understand the general scope of services, features, or technologies on the exam. The general tools and technologies in this list appear in no particular order. AWS services are grouped according to their primary functions. While some of these technologies will likely be covered more than others on the exam, the order and placement of them in this list are no indication of relative weight or importance:
+  APIs
+  Cost Explorer
+  AWS Cost and Usage Report
+  AWS Command Line Interface (CLI)
+  Elastic Load Balancers
+  Amazon EC2 instance types (for example, Reserved, On-Demand, Spot)
+  AWS global infrastructure (for example, AWS Regions, Availability Zones)
+  Infrastructure as Code (IaC)
+  Amazon Machine Images (AMIs)
+  AWS Management Console
+  AWS Marketplace
+  AWS Professional Services
+  AWS Personal Health Dashboard
+  Security groups
+  AWS Service Catalog
+  AWS Service Health Dashboard
+  Service quotas
+  AWS software development kits (SDKs)
+  AWS Support Center
+  AWS Support tiers
+  Virtual private networks (VPNs)
+AWS services and features
+Analytics:
+  Amazon Athena
+  Amazon Kinesis
+  Amazon QuickSight
+Application Integration:
+  Amazon Simple Notification Service (Amazon SNS)
+  Amazon Simple Queue Service (Amazon SQS)
+**Compute and Serverless:***
+  AWS Batch
+  **Amazon EC2***
+    AWS built datacenters, secured them, purched servers, installed servers
+
+        AWS Elastic Beanstalk
+  AWS Lambda
+  Amazon Lightsail
+  Amazon WorkSpaces
+Containers:
+  Amazon Elastic Container Service (Amazon ECS)
+  Amazon Elastic Kubernetes Service (Amazon EKS)
+  AWS Fargate
+Database:
+  Amazon Aurora
+  Amazon DynamoDB
+  Amazon ElastiCache
+  Amazon RDS
+  Amazon Redshift
+Developer Tools:
+  AWS CodeBuild
+  AWS CodeCommit
+  AWS CodeDeploy
+  AWS CodePipeline
+  AWS CodeStar
+Customer Engagement:
+  Amazon Connect
+Management, Monitoring, and Governance:
+  AWS Auto Scaling
+  AWS Budgets
+  AWS CloudFormation
+  AWS CloudTrail
+  Amazon CloudWatch
+  AWS Config
+  AWS Cost and Usage Report
+  Amazon EventBridge (Amazon CloudWatch Events)
+  AWS License Manager
+  AWS Managed Services
+  AWS Organizations
+  AWS Secrets Manager
+  AWS Systems Manager
+  AWS Systems Manager Parameter Store
+  AWS Trusted Advisor
+Networking and Content Delivery:
+  Amazon API Gateway
+  Amazon CloudFront
+  AWS Direct Connect
+  Amazon Route 53
+  Amazon VPC
+
+ Security, Identity, and Compliance:
+  AWS Artifact
+  AWS Certificate Manager (ACM)
+  AWS CloudHSM
+  Amazon Cognito
+  Amazon Detective
+  Amazon GuardDuty
+  AWS Identity and Access Management (IAM)
+  Amazon Inspector
+  AWS License Manager
+  Amazon Macie
+  AWS Shield
+  AWS WAF
+Storage:
+  AWS Backup
+  Amazon Elastic Block Store (Amazon EBS)
+  Amazon Elastic File System (Amazon EFS)
+  Amazon S3
+  Amazon S3 Glacier
+  AWS Snowball Edge
+  AWS Storage Gateway
+`,
+  },
+
+  empty: {
+      title: 'AWS Cloud Practitioner Essentials',
+      related: [],
+      text: `
+What is cloud computing?
+    - On-demand delivery of IT resources and applications through the internet with pay-as-you-go pricing
+      `
+  }
 
   // empty: {
   //     title: '',
